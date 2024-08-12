230b1d8ad60b5428771ae44f2f33bba8441668d6
Bekzod Uskanboyev <uskanboyevbekzod@gmail.com>
2024-08-12 15:55:48 +0500

    comitted

--- index.php
+++ index.php
@@ -2,3 +2,9 @@
 
 declare(strict_types=1);
 
+require 'vendor/autoload.php';
+require 'bootstrap.php';
+
+$User = new \RentApp\User();
+
+$User->delete(1);
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["public/**/*.{html,js,jsx,ts,tsx,php}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
