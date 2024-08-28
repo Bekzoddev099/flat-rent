0b03e2f3a4229e38e398e3b91df3e67b632e1f0d
Bekzod Uskanboyev <uskanboyevbekzod@gmail.com>
2024-08-28 15:11:24 +0500

    new committed

--- public/index.php
+++ public/index.php
@@ -3,10 +3,3 @@
 require "../bootstrap.php";
 require "../helpers.php";
 require "../router.php";
-
-/**
- * TODO:
- *  1. Fix upload files
- *   1.1 Rename: unique_string + original_name
- *  2. Login/Register pages
- */
